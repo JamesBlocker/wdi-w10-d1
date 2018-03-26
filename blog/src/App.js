9f44e96400bc8181a0915eeead27d14c6b332bcc
Byron <jamesblocker81@gmail.com>
2018-03-26 15:59:59 -0700

    add img

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from './Navbar.js'
 import PostsConatiner from './PostsContainer.js'
+import puppyImage from './puppy.jpg'
 import './App.css'
 
 const App = () => {
     return (
         <div>
             <Navbar />
+            <img src={'https://www.cesarsway.com/sites/newcesarsway/files/styles/large_article_preview/public/All-about-puppies--Cesar%E2%80%99s-tips%2C-tricks-and-advice.jpg?itok=bi9xUvwe'} alt="Puppy" />
             <PostsConatiner />
         </div>
     )
