e49dbcd86fddd12b78f0d7691e41cd42b1425956
Byron <jamesblocker81@gmail.com>
2018-03-26 15:32:18 -0700

    update ID's

--- blog/src/SinglePost.js
+++ blog/src/SinglePost.js
@@ -2,10 +2,12 @@ import React from 'react'
 
 const SinglePost = (props) => {
     console.log(props)
+    const post = props.post
     return (
         <div className="SinglePost">
-            <h1>{props.post.title}</h1>
-            <p>{props.post.body}</p>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
+            <small>{"ID: " + post._id}</small>
         </div>
     )
 }
